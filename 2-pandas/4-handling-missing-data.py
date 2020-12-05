cd3142e50a082770d9b4eed86e2a51e0bd2f87a0
Hayrettin Basaraner <hbasaraner@gmail.com>
2020-12-06 00:30:13 +0300

    Fix

--- 2-pandas/4-handling-missing-data.py
+++ 2-pandas/4-handling-missing-data.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import numpy as np
 
-data = pd.DataFrame([[2.3, 3.3, NaN],
-                     [7.5, Nan, 9.8], [NaN, 2.2., 6.8],
-                     [5.6, 9.2, 7.4], [NaN, NaN, NaN]])
+data = pd.DataFrame([[2.3, 3.3, np.nan],
+                     [7.5, np.nan, 9.8], [np.nan, 2.2, 6.8],
+                     [5.6, 9.2, 7.4], [np.nan, np.nan, np.nan]])
 
 # 1. filtering missing data
 
@@ -18,7 +19,7 @@ print(data.dropna(how='all'))
 print(data.fillna(0))
 
 # fill null values with mean
-print(data.fillna(mean(data)))
+print(data.fillna(data.mean()))
 
 # null values for specific column
 print(data.fillna({0: 2.5, 1: 3.0, 2: 5.5}))
